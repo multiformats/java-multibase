51c2a0b75d968803acf23cb70cc6d594e15e22e9
odisseus <odisseus@users.noreply.github.com>
2020-06-16 17:01:48 +0200

    old decode

--- src/main/java/io/ipfs/multibase/Base16.java
+++ src/main/java/io/ipfs/multibase/Base16.java
@@ -3,6 +3,11 @@ package io.ipfs.multibase;
 public class Base16 {
 
     @Deprecated
+    public static byte[] decode(String hex) {
+        return BlockEncoding.Base16.decode(hex);
+    }
+
+    @Deprecated
     public static String encode(byte[] data) {
         return BlockEncoding.Base16.encode(data);
     }
