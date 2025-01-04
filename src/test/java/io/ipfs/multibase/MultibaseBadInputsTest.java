36da2de113b37f6c38f19e31511c31a905593c95
Michael Vorburger <mike@vorburger.ch>
2025-01-04 21:32:25 +0100

    Mechanical reformat (only) of MultibaseBadInputsTest with google-java-format

--- src/test/java/io/ipfs/multibase/MultibaseBadInputsTest.java
+++ src/test/java/io/ipfs/multibase/MultibaseBadInputsTest.java
@@ -1,22 +1,22 @@
 package io.ipfs.multibase;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class MultibaseBadInputsTest {
 
     public static Collection<String> data() {
         return Arrays.asList(
-            "f012", // Hex string of odd length, not allowed in Base16
-            "f0g", // 'g' char is not allowed in Base16
-            "zt1Zv2yaI", // 'I' char is not allowed in Base58
-            "2", // '2' is not a valid encoding marker
-            "" // Empty string is not a valid multibase
+                "f012", // Hex string of odd length, not allowed in Base16
+                "f0g", // 'g' char is not allowed in Base16
+                "zt1Zv2yaI", // 'I' char is not allowed in Base58
+                "2", // '2' is not a valid encoding marker
+                "" // Empty string is not a valid multibase
         );
     }
 
